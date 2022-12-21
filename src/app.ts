412c0ba77ec70f9b233c75a42d553165c33f4e91
profCodey <otunaiyademilade@gmail.com>
2022-12-21 01:28:05 +0100

    import all middlewares

--- src/app.ts
+++ src/app.ts
@@ -9,3 +9,50 @@ import cors from "cors";
 import droneRouter from "./routes/drone";
 import medicationRouter from "./routes/medication";
 
+
+db.sync({
+  // force: true,
+})
+  .then(() => {
+    console.log("Database connected succcessfully");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const app = express();
+
+// view engine setup
+app.use(cors());
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+// app.use('/users', userRouter);
+app.use("/drones", droneRouter);
+app.use("/medications", medicationRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (
+  err: createError.HttpError,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+export default app;
