dbcb4d0e5fdafe7a7dd76749b8c0488974f82f53
profCodey <otunaiyademilade@gmail.com>
2022-12-21 01:33:51 +0100

    created schema for medication

--- dist/model/medication.js
+++ dist/model/medication.js
@@ -1,3 +1,39 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.MedicationInstance = void 0;
+const sequelize_1 = require("sequelize");
+const database_config_1 = __importDefault(require("../config/database.config"));
+class MedicationInstance extends sequelize_1.Model {
+}
+exports.MedicationInstance = MedicationInstance;
+MedicationInstance.init({
+    id: {
+        type: sequelize_1.DataTypes.UUIDV4,
+        primaryKey: true,
+        allowNull: false,
+    },
+    name: {
+        type: sequelize_1.DataTypes.STRING,
+        allowNull: false,
+    },
+    image: {
+        type: sequelize_1.DataTypes.STRING,
+    },
+    code: {
+        type: sequelize_1.DataTypes.STRING,
+    },
+    weight: {
+        type: sequelize_1.DataTypes.STRING,
+        allowNull: false,
+    },
+    droneId: {
+        type: sequelize_1.DataTypes.STRING,
+    },
+}, {
+    sequelize: database_config_1.default,
+    tableName: "medication",
+});
 //# sourceMappingURL=medication.js.map
--- src/model/medication.ts
+++ src/model/medication.ts
@@ -1,2 +1,44 @@
 import { DataTypes, Model } from "sequelize";
 import db from "../config/database.config";
+
+interface MedicationAttribute {
+  id: string;
+  name: string;
+  image: string;
+  code: string;
+  weight: string;
+  droneId: string; 
+}
+
+
+export class MedicationInstance extends Model<MedicationAttribute> {}
+MedicationInstance.init(
+  {
+    id: {
+      type: DataTypes.UUIDV4,
+      primaryKey: true,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+    },
+    code: {
+      type: DataTypes.STRING,
+    },
+    weight: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    droneId: {
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    sequelize: db,
+    tableName: "medication",
+  }
+);
